4eebff32017ebeba8bc7d75a126f7d7b8f7c73f6
John Cutler <john@futureticketing.ie>
2020-10-05 14:23:58 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import LeftButton from "./components/LeftButton";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PREDICTOR WILL GO HERE</h1>
+      <LeftButton></LeftButton>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LeftButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const LeftButton = () => {
+  return <button className="btn-primary">HEY WHY ARE YOU A GIMP ?!</button>;
+};
+
+export default LeftButton;
